ca0a1d5fb14ecf0f1256c8552411e3a9895718b2
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-26 13:19:20 +0900

    feat: MovieService 추가
    영화 정보 관련 비동기 로직 작성
    영화 정보 한번에 갖고 오기

---
+++ src/lib/MovieService.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+const API_KEY = '7035c60c'
+
+export default class MovieService {
+	
+	static getMovies = async ({title, page}) => {
+		const response = await axios({
+			url: `https://www.omdbapi.com?apikey=${API_KEY}&s=${title}&page=${page}`,
+			method: 'GET',
+		})
+		return response.data
+	}
+}
