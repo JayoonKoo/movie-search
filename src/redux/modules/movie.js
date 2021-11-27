2e92ccefea1d5b2e2a6c371ab87da3ba89b13f4a
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-27 16:19:33 +0900

    feat: 영화 상세 정보 가져오는 movie module 추가

--- src/redux/modules/movie.js
+++ src/redux/modules/movie.js
@@ -25,6 +25,8 @@ const {
   { prefix }
 );
 
+export {clearMovie, clearMovies}
+
 const initailState = Map({
   movies: Map({
     loading: false,
@@ -40,7 +42,8 @@ const initailState = Map({
 
 const reducer = handleActions(
   {
-    GET_MOVIES_PENDING: (state) => state.setIn(["movies", "loading"], true).setIn(['movies', 'error'], null),
+    GET_MOVIES_PENDING: (state) =>
+      state.setIn(["movies", "loading"], true).setIn(["movies", "error"], null),
     GET_MOVIES_SUCCESS: (state, action) => {
       const data = fromJS(action.payload);
       if (typeof data === "undefined") {
@@ -49,10 +52,22 @@ const reducer = handleActions(
       const newState = state.setIn(["movies", "loading"], false);
       return newState.setIn(["movies", "data"], fromJS(action.payload));
     },
-    GET_MOVIES_FAIL: (state, action) => 
+    GET_MOVIES_FAIL: (state, action) =>
       state
         .setIn(["movies", "loading"], false)
-        .setIn(["movies", "error"], fromJS(action.payload))
+        .setIn(["movies", "error"], fromJS(action.payload)),
+    GET_MOVIE_PENDING: (state) =>
+      state.setIn(["movie", "loading"], true).setIn(["movie", "error"], null),
+    GET_MOVIE_SUCCESS: (state, action) =>
+      state
+        .setIn(["movie", "loading"], false)
+        .setIn(["movie", "data"], fromJS(action.payload)),
+    GET_MOVIE_FAIL: (state, action) =>
+      state
+        .setIn(["movie", "loading"], false)
+        .setIn(["movie", "error"], fromJS(action.payload)),
+		CLEAR_MOVIES: (state) => state.setIn(['movies', 'data'], null),
+		CLEAR_MOVIE: (state) => state.setIn(['movie', 'data'], null),
   },
   initailState,
   { prefix }
@@ -63,18 +78,28 @@ export default reducer;
 // thunk
 export const getMoviesThunk =
   ({ title, page = 1 }) =>
-  async (dispatch, state) => {
+  async (dispatch) => {
     dispatch(getMoviesPending());
     try {
       const movies = await MovieService.getMovies({ title, page });
       dispatch(getMoviesSuccess(movies.Search));
     } catch (error) {
-			let errorMsg ="" 
-			if (error.response) {
-				errorMsg = error.response.data
-			} else {
-				errorMsg = error.message
-			}
+      let errorMsg = "";
+      if (error.response) {
+        errorMsg = error.response.data;
+      } else {
+        errorMsg = error.message;
+      }
       dispatch(getMoviesFail(errorMsg));
     }
   };
+
+export const getMovieThunk = ({ id }) => async (dispatch) => {
+	dispatch(getMoviePending()) 
+	try {
+		const movie = await MovieService.getMovieDetail({id})
+		dispatch(getMovieSuccess(movie))
+	} catch (error) {
+		dispatch(getMovieFail(error.response.data))
+	}
+};
